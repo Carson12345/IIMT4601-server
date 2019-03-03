fd3b4ce7c59e7d41a1ee473c2efcad6b81af6f40
carson ross <carson.yaumy@gmail.com>
2019-03-03 13:53:33 +0800

    set port

--- app.py
+++ app.py
@@ -102,4 +102,4 @@ def search():
 	return render_template('searchlist.html',todos=todos_l,t=title,h=heading)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8081, host='0.0.0.0') 
+    app.run(debug=True, port=80, host='0.0.0.0') 
