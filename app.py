0a5070649d9625220f3128721ce33302cec95433
carson ross <carson.yaumy@gmail.com>
2019-03-23 14:42:04 +0800

    test

--- app.py
+++ app.py
@@ -142,6 +142,7 @@ def findAuthor():
 	print(request.values.get("names"))
 	print(request.form)
 	print(request.form.get("names"))
+	print(request.form.getlist('name[]'))
 	# names = request.json['names']
 	for author in names:
 		print('found author: ' + author)
